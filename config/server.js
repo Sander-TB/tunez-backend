ee8e41bce9882de1befeb8142017ec8235de611b
Sander Byrkjeland <sunbyr@gmail.com>
2021-10-26 22:23:16 +0200

    initialized backend repo for Tunez-website

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da317b2267d5e3ceec423ec99e03a5af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a69e33de-6faf-434d-9475-31bc5132ac77'
+};
